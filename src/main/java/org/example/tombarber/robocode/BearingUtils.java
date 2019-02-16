42099c39816a9002429156e07abfd7adc8d58387
Tom Barber <tom.a.barber@gmail.com>
2019-02-16 19:08:01 +0000

    BearingUtils - able to calculate absolute bearings

--- src/main/java/org/example/tombarber/robocode/BearingUtils.java
+++ src/main/java/org/example/tombarber/robocode/BearingUtils.java
@@ -4,20 +4,27 @@ import java.awt.geom.Point2D;
 
 public class BearingUtils {
     public static double getAbsoluteBearing(double x1, double y1, double x2, double y2) {
-        double xo = x2-x1;
-        double yo = y2-y1;
+        double xo = x2 - x1;
+        double yo = y2 - y1;
         double hyp = Point2D.distance(x1, y1, x2, y2);
         double arcSin = Math.toDegrees(Math.asin(xo / hyp));
-        double bearing = 0;
 
-        if (xo > 0 && yo > 0) { // both pos: lower-Left
+        double bearing = 0; // north
+
+        if (xo > 0 && yo > 0) { // both pos: 0 < target bearing < 90
             bearing = arcSin;
-        } else if (xo < 0 && yo > 0) { // x neg, y pos: lower-right
-            bearing = 360 + arcSin; // arcsin is negative here, actuall 360 - ang
-        } else if (xo > 0 && yo < 0) { // x pos, y neg: upper-left
+        } else if (xo > 0 && yo == 0) { //directly "east"
+            bearing = 90.0;
+        } else if (xo > 0 && yo < 0) { // x pos, y neg: 90 < target bearing < 180
             bearing = 180 - arcSin;
-        } else if (xo < 0 && yo < 0) { // both neg: upper-right
+        } else if (xo == 0 && yo < 0) { //directly "south"
+            bearing = 180.0;
+        } else if (xo < 0 && yo < 0) { // both neg: 180 < target bearing < 270
             bearing = 180 - arcSin; // arcsin is negative here, actually 180 + ang
+        } else if (xo < 0 && yo == 0) { // directly "west"
+            bearing = 270.0;
+        } else if (xo < 0 && yo > 0) { // x neg, y pos: 270 < target bearing < 360
+            bearing = 360 + arcSin; // arcsin is negative here, actuall 360 - ang
         }
 
         return bearing;
@@ -25,7 +32,7 @@ public class BearingUtils {
 
     // normalizes a bearing to between +180 and -180
     public static double normalizeBearing(double angle) {
-        while (angle >  180) angle -= 360;
+        while (angle > 180) angle -= 360;
         while (angle < -180) angle += 360;
         return angle;
     }
--- src/test/java/org/example/tombarber/robocode/BearingUtilsTest.java
+++ src/test/java/org/example/tombarber/robocode/BearingUtilsTest.java
@@ -1,10 +1,13 @@
 package org.example.tombarber.robocode;
 
+import org.assertj.core.data.Offset;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BearingUtilsTest {
+
+    private static final Offset<Double> OFFSET = Offset.offset(0.00001);
 
     @Test
     void willNormaliseBearingBetweenZeroAnd180() {
@@ -34,5 +37,45 @@ public class BearingUtilsTest {
     @Test
     void willNormaliseBearingWayUnderMinus180() {
         assertThat(BearingUtils.normalizeBearing(-181.0 - 360 - 360)).isEqualTo(179.0);
+    }
+
+    @Test
+    void canCalculateBearingToEnemyWithSameXGreaterY() {
+        assertThat(BearingUtils.getAbsoluteBearing(1.0, 1.0, 1.0, 2.0)).isEqualTo(0.0, OFFSET);
+    }
+
+    @Test
+    void canCalculateBearingToEnemyWithGreaterXAndGreaterY() {
+        assertThat(BearingUtils.getAbsoluteBearing(1.0, 1.0, 2.0, 2.0)).isEqualTo(45.0, OFFSET);
+    }
+
+    @Test
+    void canCalculateBearingToEnemyWithGreaterXAndSameY() {
+        assertThat(BearingUtils.getAbsoluteBearing(1.0, 1.0, 2.0, 1.0)).isEqualTo(90.0, OFFSET);
+    }
+
+    @Test
+    void canCalculateBearingToEnemyWithGreaterXAndLowerY() {
+        assertThat(BearingUtils.getAbsoluteBearing(1.0, 2.0, 2.0, 1.0)).isEqualTo(135.0, OFFSET);
+    }
+
+    @Test
+    void canCalculateBearingToEnemyWithSameXAndLowerY() {
+        assertThat(BearingUtils.getAbsoluteBearing(1.0, 2.0, 1.0, 1.0)).isEqualTo(180.0, OFFSET);
+    }
+
+    @Test
+    void canCalculateBearingToEnemyWithLowerXAndLowerY() {
+        assertThat(BearingUtils.getAbsoluteBearing(2.0, 2.0, 1.0, 1.0)).isEqualTo(225.0, OFFSET);
+    }
+
+    @Test
+    void canCalculateBearingToEnemyWithLowerXAndSameY() {
+        assertThat(BearingUtils.getAbsoluteBearing(2.0, 2.0, 1.0, 2.0)).isEqualTo(270.0, OFFSET);
+    }
+
+    @Test
+    void canCalculateBearingToEnemyWithLowerXAndGreaterY() {
+        assertThat(BearingUtils.getAbsoluteBearing(2.0, 1.0, 1.0, 2.0)).isEqualTo(315.0, OFFSET);
     }
 }
